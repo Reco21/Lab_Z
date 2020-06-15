066a3f45eee3c33f5356629174a41e8427697240
Reco <reco754@gmail.com>
2020-06-15 16:11:18 +0200

    #1 Added Main

--- src/com/labZ/Main.java
+++ src/com/labZ/Main.java
@@ -3,6 +3,8 @@ package com.labZ;
 public class Main {
 
     public static void main(String[] args) {
-	// Zmiana w main
+        System.out.println("Hellp, World");
+        System.out.println("ZPI - GIT");
+        System.out.println("Nowy 'feature' jak to nazywaja programisci");
     }
 }
